1ba84b8e8fa465b0d517a3e6bf98d9175efbc0bd
Kjell Reigstad <kjell@kjellr.com>
2020-10-12 20:07:07 -0400

    Add paragraph warp filter.

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ if ( function_exists( 'register_block_style' ) ) {
 		 * Register block styles
 		 */
 
-		// Heading
+		// Heading (Warped)
 		register_block_style(
 			'core/heading',
 			array(
@@ -119,6 +119,16 @@ if ( function_exists( 'register_block_style' ) ) {
 				'style_handle' => 'photocopier-stylesheet',
 			)
 		);
+
+		// Paragraph (Warped)
+		register_block_style(
+			'core/paragraph',
+			array(
+				'name'         => 'photocopy-warped',
+				'label'        => 'Warped',
+				'style_handle' => 'photocopier-stylesheet',
+			)
+		);
 	}
 
 	add_action( 'init', 'photocopier_register_block_styles' );
@@ -128,7 +138,7 @@ if ( function_exists( 'register_block_style' ) ) {
  * Load the distortion SVG into the footer in the front end and editor.
  */
 function photocopier_load_svg_filter() { 
-    echo "<svg class='photocopier-svg-filter'><filter id='photocopy-filter-warp' filterUnits='userSpaceOnUse' x='0' y='0'><feTurbulence baseFrequency='0 0.021' seed='1' result='photocopy-turbulance'></feTurbulence><feDisplacementMap in='SourceGraphic' in2='photocopy-turbulance' scale='38' result='photocopy-displacement'></feDisplacementMap><feOffset dx='-14' dy='-9' result='photocopy-offset' in='photocopy-displacement'></feOffset><feComposite in='photocopy-offset' in2='SourceGraphic' result='photocopy-composite'></feComposite></filter></svg><svg class='photocopier-svg-filter'><filter id='photocopy-filter-warp-text' filterUnits='userSpaceOnUse' x='0' y='0'><feTurbulence baseFrequency='0 0.021' seed='1' result='photocopy-turbulance'></feTurbulence><feDisplacementMap in='SourceGraphic' in2='photocopy-turbulance' scale='60' result='photocopy-displacement'></feDisplacementMap><feOffset dx='-14' dy='-10' result='photocopy-offset' in='photocopy-displacement'></feOffset></filter></svg>";
+    echo "<svg class='photocopier-svg-filter'><filter id='photocopy-filter-warp' filterUnits='userSpaceOnUse' x='0' y='0'><feTurbulence baseFrequency='0 0.021' seed='1' result='photocopy-turbulance'></feTurbulence><feDisplacementMap in='SourceGraphic' in2='photocopy-turbulance' scale='38' result='photocopy-displacement'></feDisplacementMap><feOffset dx='-14' dy='-9' result='photocopy-offset' in='photocopy-displacement'></feOffset><feComposite in='photocopy-offset' in2='SourceGraphic' result='photocopy-composite'></feComposite></filter></svg><svg class='photocopier-svg-filter'><filter id='photocopy-filter-warp-text' filterUnits='userSpaceOnUse' x='0' y='0'><feTurbulence baseFrequency='0 0.01512' seed='1' result='photocopy-turbulance'></feTurbulence><feDisplacementMap in='SourceGraphic' in2='photocopy-turbulance' scale='100' result='photocopy-displacement'></feDisplacementMap><feOffset dx='-20' dy='-20' result='photocopy-offset' in='photocopy-displacement'></feOffset></filter></svg>";
 }
 add_action('wp_footer', 'photocopier_load_svg_filter');
 add_action( 'admin_footer-post.php', 'photocopier_load_svg_filter' );
